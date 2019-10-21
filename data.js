2457a4eee9abbfdeae51bb662c2bdb8bd667c822
ELITS UFPB <elitsufpb1@gmail.com>
2019-10-20 21:27:44 -0300

    Post

---
+++ data.js
@@ -0,0 +1,51 @@
+var data = {
+	"quiz":
+	[
+		{
+			"questao":"Você recebeu 1000 reais e quer investir em inovação culinária, qual alternativa você escolheria?",
+			"a":"Investiria todo o valor em insumo para produção de bolos profissionais tradicionais.",
+			"b":"Investiria 30% do valor em capacitação de bolos profissionais inovadores e 70% em insumo e equipamentos profissionais.",
+			"c":"Investiria 50% em insumo e 50% em equipamentos para produção de bolos.",
+			"d":"Analisar a preferência do público-alvo, a concorrência, para então decidir o ambiente mais favorável.",
+			"p":1324,
+			"tema":"Flexibilidade cognitiva / Negociação"
+
+		},
+		{
+			"questao":"Você tem uma empresa de inovação tecnológica em saúde e precisa escolher sua equipe quais profissionais escolheria?",
+			"a":"Medicina, marketing, administração, recursos humanos.",
+			"b":"Profissionais da saúde de acordo com a necessidade, design, marketing, profissionais de tecnologia de acordo com a necessidade.",
+			"c":"Digital influencer, médico, design gráfico e administração.",
+			"d":"Médico, design, marketing, engenheiro elétrico.",
+			"p":2413,
+			"tema":"Orientação de serviço / Liderança e Gestão de pessoas"
+		},
+		{
+			"questao":"Um empregado não está com um bom desempenho, o que está gerando baixo rendimento dentro da empresa. Qual seria a sua atitude?",
+			"a":"Demitiria por justa causa e contraria um profissional melhor qualificado.",
+			"b":"Conversaria com o funcionário sobre as dores do mesmo e procuraríamos estratégias para solucionar o problema.",
+			"c":"Levaria a problemática para o RH e buscaria estratégias sem consultar o funcionário.",
+			"d":"Contataria o RH para o funcionário receber uma advertência.",
+			"p":1432,
+			"tema":"Julgamento e tomada de decisões / Resolução de problemas complexos"
+		},
+		{
+			"questao":"Você precisa escolher as características de um profissional da sua equipe:",
+			"a":"Influenciador, dominante, estável e complacente.",
+			"b":"Dominante, inconveniente, carismático e honesto.",
+			"c":"Estável, obediente, passivo e introvertido.",
+			"d":"Personalidade forte, rígido, instável e criativo.",
+			"p":4321,
+			"tema":"Inteligência Emocional / Trabalho em equipe"
+		},
+		{
+			"questao":"Você irá desenvolver um celular e precisa decidir as características necessárias: ",
+			"a":"Ótimo desempenho e baixa resolução na câmera.",
+			"b":"Menor desempenho e alta resolução na câmera.",
+			"c":"Desempenho e resolução da câmera intermediária. ",
+			"d":"Buscaria novas estratégias para otimização do produto, aumentando o período de produção.",
+			"p":2134,
+			"tema":"Pensamento crítico / Criatividade"
+		}
+	]
+};
